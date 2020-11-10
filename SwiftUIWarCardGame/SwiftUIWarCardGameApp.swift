f07db9d05abe97a484aee89a41a3973a20b2712e
jmr18c <jmr18c@acu.edu>
2020-11-10 08:26:51 -0600

    Initial Commit

---
+++ SwiftUIWarCardGame/SwiftUIWarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWarCardGameApp.swift
+//  SwiftUIWarCardGame
+//
+//  Created by Justin Raitz on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
